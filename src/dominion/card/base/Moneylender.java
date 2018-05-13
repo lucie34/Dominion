16dc6e559a2d3f3996d26475f7adeb9cc0330285
Lucie PEREZ <perez.cagnac.lucie@gmail.com>
2018-05-13 11:09:51 +0200

    test

--- src/dominion/card/base/Moneylender.java
+++ src/dominion/card/base/Moneylender.java
@@ -19,6 +19,12 @@ public class Moneylender extends ActionCard {
 	public void play(Player p) {
 		Copper carteCuivre = new Copper();
 		System.out.println("\n ne contient pas de cuivre XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		for(int i=0; i<p.cardsInHand().size(); i++ ) {
+			System.out.println("\n la main " + p.cardsInHand().get(i));
+		}
+		for(int i=0; i<p.cardsInHand().size(); i++ ) {
+			if(p.cardsInHand().get(i).equals(carteCuivre)) {System.out.println("OK xxxxxxxxxxxxxxxxxxxxxxxx");}
+		}
 		if(p.cardsInHand().contains(carteCuivre)) {
 			System.out.println("\n contient 1 cuivre XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 			p.removeFromHand(carteCuivre);
