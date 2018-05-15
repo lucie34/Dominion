ed4dc6a7164e7c65486f8efeab913443e3c1ef91
Laurine Bartoli <laurine.bartoli@hotmail.fr>
2018-05-15 08:17:47 +0200

    Maj mine

--- src/dominion/card/base/Mine.java
+++ src/dominion/card/base/Mine.java
@@ -65,38 +65,5 @@ public class Mine extends ActionCard {
 				System.out.println("Aucune carte trésor de la réserve à moins de "+(coutCarte+3)+" pièces disponible");
 			}
 		}//Si aucune carte trésor dans la main à écarter, rien
-		
-		
-		
-		
-		
-		/*CardList carteTrésor = p.getTreasureCards();
-		int indice = -1;
-		int i =0;
-		while(indice == -1) {
-			if(carteTrésor.get(i).getName().equalsIgnoreCase("Copper")) {
-				indice = i;
-			}
-			else if(carteTrésor.get(i).getName().equalsIgnoreCase("Silver")) {
-				indice = i;
-			}
-			i++;
-		}
-		if(indice != -1) {
-			Gold or = new Gold();
-			Silver argent = new Silver();
-			if(p.getGame().availableSupplyCards().contains(or) && carteTrésor.get(i).getName().equalsIgnoreCase("Silver")) {
-				Card c = p.getGame().getFromSupply("Gold");
-				p.incrementHand(c); 
-				p.getGame().removeFromSupply("Gold");
-				p.removeFromHand(carteTrésor.get(indice));
-			}
-			else if(p.getGame().availableSupplyCards().contains(argent) && carteTrésor.get(i).getName().equalsIgnoreCase("Copper")) {
-				Card c = p.getGame().getFromSupply("Silver");
-				p.incrementHand(c);; 
-				p.getGame().removeFromSupply("Silver");
-				p.removeFromHand(carteTrésor.get(indice));
-			}
-		}	// si il n'y a plus de pièces or ou argent dans la reserve on ne fait rien*/
 	}
 }
