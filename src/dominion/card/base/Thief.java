fd244699b9bcfe88d550b44faa9bb313c7a8ec43
Laurine Bartoli <laurine.bartoli@hotmail.fr>
2018-05-15 09:13:56 +0200

    Maj Thief

--- src/dominion/card/base/Thief.java
+++ src/dominion/card/base/Thief.java
@@ -9,6 +9,7 @@ import dominion.card.*;
  * Tous vos adversaires d√©voilent les 2 premi√®res cartes de leur pioche. S'ils d√©voilent des cartes Tr√©sor, ils en √©cartent 1 de votre choix. Parmi ces cartes Tr√©sor √©cart√©es, recevez celles de votre choix. Les autres cartes d√©voil√©es sont d√©fauss√©es.
  */
 public class Thief extends AttackCard {
+	private static CardList cartesEcarte = new CardList();
 	
 	public Thief() {
 		super("Thief", 4);
@@ -32,49 +33,22 @@ public class Thief extends AttackCard {
 				}
 			}
 		}
-		System.out.println("\n"+cartesTresor.toString()+"TTTTTTTTTTTTTTTTTTT 1");
-		System.out.println("\nDÈfausse P 1 : "+p.getDiscard().toString());
 		if(!cartesTresor.isEmpty()) {
-			CardList cartesEcarte = new CardList();
 			int nbCartesTresorEcarte = 0;
 			int nbCartesTresor = cartesTresor.size();
 			String instruction = joueurActif.getName()+" : Choisissez de faire Ècarter une carte trÈsor ‡ "+p.getName();
 			String rep = joueurActif.chooseCard(instruction, cartesTresor, false);
-			for(int i=0; i<nbCartesTresor; i++) {
-				if(nbCartesTresorEcarte < 1 && cartesTresor.get(i).getName().equalsIgnoreCase(rep)) {
-					nbCartesTresorEcarte++;
-					cartesEcarte.add(cartesTresor.get(i));
-					cartesTresor.remove(i);
-				}
-			}
-			System.out.println(cartesEcarte.toString()+"EEEEEEEEEEEEEEEEEEEEE 1");
-			System.out.println(cartesTresor.toString()+"TTTTTTTTTTTTTTTTTTTTT 2");
-			for(Card carte : cartesTresor) {
-				p.gain(carte);
-			}
-			System.out.println("DÈfausse P 2 : "+p.getDiscard().toString());
-			System.out.println("DÈfausse JA 1 : "+joueurActif.getDiscard().toString());
-			if(!cartesEcarte.isEmpty()) {
-				int nbCartesRecup = 0;
-				int nbCartesEcarte = cartesEcarte.size();
-				instruction = joueurActif.getName()+" : Choisissez de rÈcupÈrer une carte trÈsor ÈcartÈe ou laissez vide";
-				System.out.println("OK");
-				rep = joueurActif.chooseCard(instruction, cartesEcarte, true);
-				System.out.println("\nREP = "+rep);
-				if(!rep.equalsIgnoreCase("")) {
-					for(int i=0; i<nbCartesEcarte; i++) {
-						if(nbCartesRecup < 1 && cartesEcarte.get(i).getName().equalsIgnoreCase(rep)) {
-							nbCartesRecup++;
-							joueurActif.gain(cartesEcarte.get(i));
-							cartesEcarte.remove(i);
-						}
+			if(!rep.equalsIgnoreCase("")) {
+				for(int i=0; i<nbCartesTresor; i++) {
+					if(nbCartesTresorEcarte < 1 && cartesTresor.get(i).getName().equalsIgnoreCase(rep)) {
+						nbCartesTresorEcarte++;
+						cartesEcarte.add(cartesTresor.get(i));
+						cartesTresor.remove(i);
 					}
 				}
-				System.out.println("DÈfausse JA 2 : "+joueurActif.getDiscard().toString());
-				System.out.println(cartesEcarte.toString()+"EEEEEEEEEEEEEEEEEEEEE 2");
-				for(Card carte : cartesEcarte) {
-					p.getGame().addInTrash(carte);
-				}
+			}
+			for(Card carte : cartesTresor) {
+				p.gain(carte);
 			}
 		}
 	}
@@ -86,6 +60,24 @@ public class Thief extends AttackCard {
 			//VÈrifie que l'adversaire n'a pas dans sa main une carte Douves l'immunisant
 			if(!douves.devoiler(adversaires.get(i), adversaires.get(i).cardsInHand())) {
 				this.attaquer(adversaires.get(i));
+			}
+		}
+		if(!cartesEcarte.isEmpty()) {
+			int nbCartesRecup = 0;
+			int nbCartesEcarte = cartesEcarte.size();
+			String instruction = p.getName()+" : Choisissez de rÈcupÈrer une carte trÈsor ÈcartÈe ou laissez vide";
+			String rep2 = p.chooseCard(instruction, cartesEcarte, true);
+			if(!rep2.equalsIgnoreCase("")) {
+				for(int i=0; i<nbCartesEcarte; i++) {
+					if(nbCartesRecup < 1 && cartesEcarte.get(i).getName().equalsIgnoreCase(rep2)) {
+						nbCartesRecup++;
+						p.gain(cartesEcarte.get(i));
+						cartesEcarte.remove(i);
+					}
+				}
+			}
+			for(Card carte : cartesEcarte) {
+				p.getGame().addInTrash(carte);
 			}
 		}
 	}
